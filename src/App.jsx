077d71941cc3a86a0bda0d81bcf5bbf68759cf00
york <york4818@gmail.com>
2025-08-20 17:11:17 +0800

    測試後端位置

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   const [data, setData] = useState(null)
   // 先用 VITE_API_URL，沒有才用 localhost
   const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
+  console.log('API_BASE_URL:', API_BASE_URL);
 
   const fetchData = (zone) => {
     fetch(`${API_BASE_URL}/SightAPI?zone=${zone}`)
